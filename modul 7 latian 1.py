77f476eb8e33be3c2d0001b97aa91243842c56ac
vangguard10 <114124494+vangguard10@users.noreply.github.com>
2022-11-04 15:34:31 +0700

    Update modul 7 latian 1.py

--- modul 7 latian 1.py
+++ modul 7 latian 1.py
@@ -1,52 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  4 14:17:44 2022
-
-@author: fariz
-"""
 def penutup():
     print('''terimakasih telah menggunakan program saya.
 ig:@alfarizqiwira''')
 def pemilih():
-    angka=int(input('masukan angka: '))
-    h=0
-    if angka == 1:
-        print('bukan bilangan prima')
-    elif angka in range(2,4):
-        print('bilangan prima')
-    elif angka > 0:
-        i=2
-        while i in range(2,angka):
-            bil = angka%i
-            if bil == 0:
-                h=h+1
-                i+=1
-            elif bil > 0:
-                h=h
-                i+=1
-        
-        if h == 0:
-            print('bilangan prima')
-        elif h > 0:
-        
+    o=1
+    while o==1:
+        angka=(input('masukan angka: '))
+        h=0    
+        if angka == '':
+            o=2
+        elif int(angka) == 1:
             print('bukan bilangan prima')
-    elif angka <= 0:
-        print('bilangan harus diatas 0 (positif)')
- 
-o=1
-while o==1:
-    print('program penentuan bilangan prima')
-    pemilih()
-         
-    b=1
-    while b == 1:
-        jwbn=input('apakah ingin mengulanginya lagi?[Y/N]')
-        if jwbn.upper() =='Y':
-            b=0
-            o=1
-        elif jwbn.upper() =='N':
-            b=0
-            o=0
-        else:
-            b=1
+        elif int(angka) in range(2,4):
+            print('bilangan prima')
+        elif int(angka) > 0:
+            i=2
+            while i in range(2,int(angka)):
+                bil = int(angka)%i
+                if bil == 0:
+                    h=h+1
+                    i+=1
+                elif bil > 0:
+                    h=h
+                    i+=1        
+            if h == 0:
+                print('bilangan prima')
+            elif h > 0:            
+                print('bukan bilangan prima')
+        elif int(angka) <= 0:
+            print('bilangan harus diatas 0 (positif)')
+        elif angka == '':
+            o=2
+print('program penentuan bilangan prima')
+pemilih()
 penutup()
